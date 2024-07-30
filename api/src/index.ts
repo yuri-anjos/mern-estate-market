b6ae06c6246a4752e27fc1bc21fe88735b209b61
yuri-anjos <yuri.dos.4njos@gmail.com>
2024-07-30 20:09:30 -0300

    connect to mongo database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,17 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const express = require("express");
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGODB_URI as string)
+	.then(() => {
+		console.log("Connected to MongoDB!");
+	})
+	.catch((error) => {
+		console.error("Error connecting to MongoDB:", error);
+	});
 
 const port = 3000;
 const app = express();
@@ -10,5 +21,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}!`);
+	console.log(`Server is running on port ${process.env.PORT}!`);
 });
